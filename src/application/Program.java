0b5bc5eaf94f2828ee836d5ea0b69afa5d8a2c54
João Paulo <joaowais@gmail.com>
2024-06-26 23:30:59 -0300

    Genéricos Delimitados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,44 @@
 package application;
 
-import services.PrintService;
+import entities.Product;
+import services.CalculationService;
 
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        PrintService<Integer> service = new PrintService();
+        List<Product> list = new ArrayList<>();
 
-        System.out.print("How many values? ");
-        int n = sc.nextInt();
-        for(int i = 0; i < n ; i++){
-            int value = sc.nextInt();
-            service.addValue(value);
+        String path = "E:\\DEVELOPMENT\\Java\\Projetos\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            Product x = CalculationService.max(list);
+            System.out.println("Most Expensive: ");
+            System.out.println(x);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            sc.close();
+
         }
 
-        service.print();
-        System.out.println("First: " + service.first());
-
-        sc.close();
     }
 }
